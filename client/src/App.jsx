553ddfcfd06b757323bcd6b775257cc94f9189bc
Nikhilesh0231 <tnikhilesh0231@gmail.com>
2025-02-22 20:46:36 +0530

    new 1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
-import React, { lazy } from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProtectRoute from "./components/auth/ProtectRoute";
+import LayoutLoader from "./components/layout/Loaders";
 const Home = lazy(() => import("./pages/Home"));
 const Login = lazy(() => import("./pages/Login"));
 const Chat = lazy(() => import("./pages/Chat"));
@@ -10,25 +11,25 @@ const NotFound = lazy(() => import("./pages/NotFound"));
 let user = true;
 
 function App() {
-  return (
+  return ( 
     <BrowserRouter>
-      <Routes>
-        <Route element={<ProtectRoute user = {user}/>}>
-          <Route path="/" element={<Home />} />
-          <Route path="/chat/:chatId" element={<Chat />} />
-          <Route path="/groups" element={<Groups />} />
-        </Route>
-        <Route path="/login" element={
-          <ProtectRoute user={!user} redirect="/"> 
-            <Login />
-          </ProtectRoute>
-          } />
-
-      <Route path="*" element={<NotFound/>} />
-        
-      </Routes>
+      <Suspense fallback={<LayoutLoader/>}>
+        <Routes>
+          <Route element={<ProtectRoute user={user} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/chat/:chatI d" element={<Chat />} />
+            <Route path="/groups" element={<Groups />} />
+          </Route>
+          <Route path="/login" element=
+            { <ProtectRoute user={!user} redirect="/">
+                <Login />
+              </ProtectRoute>
+            }/>
+          <Route path="*" element={<NotFound />}/>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
-  ); 
+  );
 }
 
 export default App;
--- client/src/components/layout/Header.jsx
+++ client/src/components/layout/Header.jsx
@@ -1,72 +1,91 @@
-import {
-  AppBar,
-  Box,
-  IconButton,
-  Toolbar,
-  Tooltip,
-  Typography,
-} from "@mui/material";
-import { Add, Menu, Search } from "@mui/icons-material";
-import React from "react";
+import {AppBar,Backdrop,Box,IconButton,Toolbar,Tooltip,Typography,} from "@mui/material";
+import { Add, Group, Logout, Menu, Notifications, Search } from "@mui/icons-material";
+import React, { lazy, Suspense, useState } from "react";
 import { orange } from "../constants/color.js";
+import { useNavigate } from "react-router-dom";
+ 
+const SearchDialog = lazy(() => import("../specific/Search.jsx")); 
+const NotificationDialog = lazy(() => import("../specific/Notifications.jsx")) 
+const NewGroupDialog = lazy(()=>import("../specific/NewGroup.jsx"))
 
 const Header = () => {
+  const navigate = useNavigate();
+
+  const[ismobile,setIsMobile] =useState(false);
+  const[isSearch,setIsSearch] =useState(false);
+  const[isNewGroup,setIsNewGroup] =useState(false);
+  const[isNotification,setIsNotification] = useState(false);
+
   const handleMobile = () => {
-    console.log("mobile");
+    setIsMobile(!ismobile);
   };
-  const openSearchDialog = () => {
-    console.log("openSearchDialog");
+  const openSearch = () => {
+    setIsSearch(!isSearch);
   };
   const openNewGroup = () => {
-    console.log("openNewGroup");
+    setIsNewGroup(!isNewGroup);
   };
+  const openNotification = () => {
+    setIsNotification(!isNotification);
+  };
+
+  const logoutHandler = () => {
+    console.log("Logout")
+  }
+
+  const navigateToGroup = () => navigate("/groups");
+
   return (
     <>
       <Box sx={{ flexGrow: 1 }} height={"4rem"}>
         <AppBar position="static" sx={{ backgroundColor: orange }}>
           <Toolbar>
-            <Typography
-              variant="h6"
-              sx={{
-                display: { xs: "none", sm: "block" },
-              }}
-            >
-              NowChatApp
-            </Typography>
-            <Box
-              sx={{
-                display: { xs: "block", sm: "none" },
-              }}
-            >
+            <Typography variant="h6" sx={{display: { xs: "none", sm: "block" },}}>NowChatApp</Typography>
+            <Box sx={{display: { xs: "block", sm: "none" },}}>
               <IconButton color="inherit" onClick={handleMobile}>
                 <Menu />
               </IconButton>
             </Box>
-            <Box
-              sx={{
-                flexGrow: 1,
-              }}
-            />
+            <Box sx={{flexGrow: 1,}}/>
             <Box>
-            <Tooltip title="Search">
-              <IconButton
-                color="inherit"
-                size="large"
-                onClick={openSearchDialog}
-              >
-                <Search />
-              </IconButton>
-              </Tooltip> 
-              <Tooltip title="New Group">
-                <IconButton color="inherit" size="large" onClick={openNewGroup}>
-                  <Add />
-                </IconButton>
-              </Tooltip>
+              <IconBtn title={"serach"} icon={<Search/>} onclick={openSearch}/>
+              <IconBtn title={"New Group"} icon={<Add/>} onclick={openNewGroup}/>
+              <IconBtn title={"Manage Groups"} icon={<Group />} onclick={navigateToGroup}/>
+              <IconBtn title={"Notifications"} icon={<Notifications/>} onclick={openNotification}/>
+              <IconBtn title={"Logout"} icon={<Logout />} onclick={logoutHandler}/>
             </Box>
           </Toolbar>
-        </AppBar>  
+        </AppBar>
       </Box>
+      {isSearch &&
+        (
+          <Suspense fallback ={<Backdrop open/>}>
+            <SearchDialog/>
+          </Suspense>
+        )}
+      {isNotification &&
+        (
+          <Suspense fallback ={<Backdrop open/>}>
+            <NotificationDialog/>
+          </Suspense>
+        )}
+      {isNewGroup &&
+        (
+          <Suspense fallback ={<Backdrop open/>}>
+            <NewGroupDialog />
+          </Suspense>
+        )}
     </>
+  );
+};
+
+const IconBtn = ({ title, icon, onclick }) => {
+  return (
+    <Tooltip title={title}>
+      <IconButton color="inherit" size="large" onClick={onclick}>
+        {icon}
+      </IconButton>
+    </Tooltip>
   );
 };
 
