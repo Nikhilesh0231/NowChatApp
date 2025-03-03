c2fbd0b2f76bb3e8238f283e32a4430c21b49f95
Nikhilesh0231 <tnikhilesh0231@gmail.com>
2025-03-04 00:33:05 +0530

    new 04march

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { isAuthenticated } from '../middlewares/auth.js';
-import { newGroupChat } from '../controllers/chat.js';
+import { getMyChats, newGroupChat } from '../controllers/chat.js';
 
 const app = express.Router();
 
@@ -8,7 +8,8 @@ const app = express.Router();
 //After here user must be logged in to access the routes
 app.use(isAuthenticated);
 
-app.post("/new",newGroupChat)
+app.post("/new",newGroupChat);
+app.get("/my",getMyChats);
 
 
 export default app; 
